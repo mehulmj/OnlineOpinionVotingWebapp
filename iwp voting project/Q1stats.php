567518dd926e85b863933089db51d5ea226aff26
Mehul Jain <31754262+mehulmj@users.noreply.github.com>
2020-11-30 21:42:42 +0530

    Update Q1stats.php

--- iwp voting project/Q1stats.php
+++ iwp voting project/Q1stats.php
@@ -5,7 +5,7 @@
 <body align="center">
 <h1>Get Stats<h1>
 <input type="button" name="Q1" value="Q1"/>
-<input type="button" name="Q2" value="Q2"/><a href="/var/httpd/html/iwp%20voting%20project/chartQ1.php"></a>
+<input type="button" name="Q2" value="Q2"/><a href="iwp%20voting%20project/chartQ1.php"></a>
 <input type="button" name="Q3" value="Q3"/>
 <input type="button" name="Q4" value="Q4"/>
 <input type="button" name="Q5" value="Q5"/>
